49aab6c20e88ccea3b65fe6ae9d632951122ee66
Luigi Bifulco <bifulcoluigi@gmail.com>
2025-09-20 11:00:42 +0200

    Add Prettier configuration and ESLint integration for consistent code formatting

--- src/frontend/chatbot-ai-fe/eslint.config.js
+++ src/frontend/chatbot-ai-fe/eslint.config.js
@@ -3,21 +3,29 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import {defineConfig, globalIgnores} from 'eslint/config'
 
 export default defineConfig([
-  globalIgnores(['dist']),
-  {
-    files: ['**/*.{ts,tsx}'],
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs['recommended-latest'],
-      reactRefresh.configs.vite,
-    ],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
+    globalIgnores(['dist']),
+    {
+        files: ['**/*.{ts,tsx}'],
+        extends: [
+            js.configs.recommended,
+            tseslint.configs.recommended,
+            reactHooks.configs['recommended-latest'],
+            reactRefresh.configs.vite,
+            'plugin:prettier/recommended',
+
+        ],
+        plugins: {
+            prettier: await import('eslint-plugin-prettier'),
+        },
+        rules: {
+            'prettier/prettier': 'warn',
+        },
+        languageOptions: {
+            ecmaVersion: 2020,
+            globals: globals.browser,
+        },
     },
-  },
 ])
